b218535a158dcbfb2bedca41be6f0896f247d755
Michael Oates <michael@3wks.com.au>
2018-08-03 13:38:45 +1000

    Require 'super' role to setup gmail (i.e. be logged in as admin@3wks.com.au)

--- src/gmail/gmail.controller.ts
+++ src/gmail/gmail.controller.ts
@@ -1,27 +1,22 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { AllowAnonymous } from '../auth/auth.guard';
+import {Roles} from '../auth/auth.guard';
 import * as passport from 'passport';
 import { GmailConfigurer } from './gmail.configurer';
-import { GmailSender } from './gmail.sender';
-import { Context } from '..';
-import { Ctxt } from '../datastore/context';
 
+@Roles("super")
 @Controller('system/gmail')
 export class GmailController {
   constructor(
     private readonly gmailConfigurer: GmailConfigurer,
-    private readonly gmailSender: GmailSender,
   ) {}
 
-  @AllowAnonymous()
   @Get('setup')
   setupGmailOAuth(@Req() request: Request, @Res() response: Response) {
     const authenticateRet = this.gmailConfigurer.authenticate();
     authenticateRet(request, response);
   }
 
-  @AllowAnonymous()
   @Get('setup/oauth2callback')
   oauthCallback(@Req() request: Request, @Res() response: Response) {
     const authenticateRet = passport.authenticate('google-gmail', {
@@ -35,21 +30,5 @@ export class GmailController {
         response.send('Gmail OAuth setup FAILED.');
       }
     });
-  }
-
-  @AllowAnonymous()
-  @Get('test-send-email')
-  testSendEmail(@Ctxt() context: Context) {
-    const mailOptions = {
-      from: 'deline@3wks.com.au',
-      to: 'deline.neo@growthops.com.au',
-      subject: 'this is the subject line',
-      text: 'It works...blah',
-      html: '<b>It works...blah</b>',
-    };
-
-    this.gmailSender.send(context, mailOptions);
-
-    return 'testing';
   }
 }
