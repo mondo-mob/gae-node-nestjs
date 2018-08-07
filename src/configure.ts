45db046bea1419016d2fbd5d9efe516addaedd69
Nick Pantelios <nickp@3wks.com.au>
2018-08-07 20:30:27 +1000

    Secure cookie for app engine

--- src/configure.ts
+++ src/configure.ts
@@ -1,10 +1,11 @@
-import * as DatastoreStore from "@google-cloud/connect-datastore";
-import * as Datastore from "@google-cloud/datastore";
-import {OneOrMany} from "@google-cloud/datastore/entity";
-import * as session from "express-session";
-import * as passport from "passport";
-import {CsrfValidator} from "./auth/csrf.interceptor";
+import * as DatastoreStore from '@google-cloud/connect-datastore';
+import * as Datastore from '@google-cloud/datastore';
+import {OneOrMany} from '@google-cloud/datastore/entity';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import {CsrfValidator} from './auth/csrf.interceptor';
 import * as csp from 'helmet-csp';
+import {rootLogger} from './gcloud/logging';
 
 interface ServerOptions {
   csp?: object,
@@ -21,26 +22,39 @@ interface ServerOptions {
 interface Express {
   use(...handlers: Function[]): void;
   use(paths: OneOrMany<string | RegExp>, ...handlers: Function[]): void;
+  set(property: string, value: boolean): any;
 }
 
 export const configureExpress = (
   expressApp: Express,
-  options: ServerOptions
+  options: ServerOptions,
 ) => {
   const SessionStore = DatastoreStore(session);
 
   expressApp.use(
     csp(options.csp || {
       directives: {
-        defaultSrc: ["'none'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-        imgSrc: ["'self'"],
-        connectSrc: ["'self'"],
+        defaultSrc: ['\'none\''],
+        scriptSrc: ['\'self\''],
+        styleSrc: ['\'self\'', '\'unsafe-inline\'', 'https://fonts.googleapis.com'],
+        fontSrc: ['\'self\'', 'https://fonts.gstatic.com'],
+        imgSrc: ['\'self\''],
+        connectSrc: ['\'self\''],
       },
     }),
   );
+
+  rootLogger.info('process.env.APP_ENGINE_ENVIRONMENT:', process.env.APP_ENGINE_ENVIRONMENT);
+
+  const cookieConfig: { secure: boolean | 'auto' } = { secure: false };
+  rootLogger.info('Cookie config before:', cookieConfig);
+
+  if (process.env.NODE_ENV === 'production') {
+    rootLogger.info('We in prod now');
+    expressApp.set('trust proxy', true);
+    cookieConfig.secure = true;
+  }
+  rootLogger.info('Cookie config after:', cookieConfig);
 
   expressApp.use(
     session({
@@ -48,13 +62,14 @@ export const configureExpress = (
       resave: false,
       store: new SessionStore({
         dataset: new Datastore({
-          prefix: "express-sessions",
+          prefix: 'express-sessions',
           apiEndpoint: options.session.apiEndpoint,
-          projectId: options.session.projectId
-        } as any)
+          projectId: options.session.projectId,
+        } as any),
       }),
-      secret: options.session.secret
-    })
+      secret: options.session.secret,
+      cookie: cookieConfig,
+    }),
   );
 
   const { ignorePaths = [/^\/(?!tasks\/|system\/).*/] } = options.csrf || {};
