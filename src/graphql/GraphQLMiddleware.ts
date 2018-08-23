2b312befcc337906bf0f3189441d4d040e5b5fc6
Glenn Molnar <glenn@3wks.com.au>
2018-08-23 14:27:03 +1000

    add support for apollo multipart file upload

--- src/graphql/GraphQLMiddleware.ts
+++ src/graphql/GraphQLMiddleware.ts
@@ -2,6 +2,7 @@ import {Injectable, MiddlewareFunction, NestMiddleware} from '@nestjs/common';
 import {GraphQLFactory} from '@nestjs/graphql';
 import {graphqlExpress} from 'apollo-server-express';
 import {GraphQLDateTime, GraphQLTime} from 'graphql-iso-date';
+import {GraphQLUpload} from 'apollo-upload-server';
 import * as _ from 'lodash';
 import {fileLoader, mergeTypes} from 'merge-graphql-schemas';
 import {rootLogger} from '../index';
@@ -24,6 +25,7 @@ export class GraphQLMiddleware implements NestMiddleware {
       resolvers: {
         Time: GraphQLTime,
         DateAndTime: GraphQLDateTime,
+        Upload: GraphQLUpload,
       },
       logger: {
         log: payload => {
--- typings/custom-types.d.ts
+++ typings/custom-types.d.ts
@@ -1,3 +1,4 @@
 declare module '@google-cloud/connect-datastore';
 declare module 'helmet-csp';
 declare module 'merge-graphql-schemas';
+declare module 'apollo-upload-server';
