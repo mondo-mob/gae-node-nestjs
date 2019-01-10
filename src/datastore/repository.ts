1fc5a53cbfa33b5583d4bea6d7e0a2a2c2a9cd77
Matt Byrne <matt@3wks.com.au>
2019-01-11 08:23:38 +1100

    Reindex does not always require an operation parameter ... by default the operation will return an unchanged entity.

--- src/datastore/repository.ts
+++ src/datastore/repository.ts
@@ -191,9 +191,10 @@ export class Repository<T extends { id: string }> {
    * Loads all entities into memory and applies some mutation to them before resaving them
    *
    * @param context
-   * @param operation
+   * @param operation (Optional) The operation to perform on each entity, returning the new
+   * form. By default this will return the same instance.
    */
-  async reindex(context: Context, operation: (input: T) => T | Promise<T>) {
+  async reindex(context: Context, operation: ((input: T) => T | Promise<T>) = input => input) {
     const [allEntities] = await this.query(context);
 
     const updatedEntities = await Promise.all(allEntities.map(operation));
