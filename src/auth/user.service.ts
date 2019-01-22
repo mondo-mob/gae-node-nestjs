2a607b03427a74854cf2746fb56092758a842c10
Matt Byrne <matt@3wks.com.au>
2019-01-23 10:10:24 +1100

    UserService optional custom update and create types
    Allow user service implementation to optionally specify the type for updates and the type for create requests.

--- src/auth/user.service.ts
+++ src/auth/user.service.ts
@@ -1,24 +1,29 @@
-import {Context, IUser, IUserCreateRequest, IUserUpdates, Transactional} from '..';
-import {LoginIdentifierRepository} from './login-identifier.repository';
+import { Context, IUser, IUserCreateRequest, IUserUpdates, Transactional } from '..';
+import { LoginIdentifierRepository } from './login-identifier.repository';
 
 export const USER_SERVICE = 'UserService';
 
-export interface UserService<T extends IUser> {
+export interface UserService<
+  T extends IUser,
+  U extends IUserUpdates = IUserUpdates,
+  C extends IUserCreateRequest = IUserCreateRequest
+> {
   getByEmail(context: Context, email: string): Promise<T | undefined>;
   get(context: Context, userId: string): Promise<T | undefined>;
-  create(context: Context, user: IUserCreateRequest): Promise<T>;
-  update(context: Context, id: string, updates: IUserUpdates): Promise<T>;
-
+  create(context: Context, user: C): Promise<T>;
+  update(context: Context, id: string, updates: U): Promise<T>;
 }
 
-export abstract class AbstractUserService<T extends IUser> implements UserService<T> {
-
-  protected constructor(protected readonly loginIdentifierRepository: LoginIdentifierRepository){
-  };
+export abstract class AbstractUserService<
+  T extends IUser,
+  U extends IUserUpdates = IUserUpdates,
+  C extends IUserCreateRequest = IUserCreateRequest
+> implements UserService<T, U, C> {
+  protected constructor(protected readonly loginIdentifierRepository: LoginIdentifierRepository) {}
 
   abstract get(context: Context, userId: string): Promise<T | undefined>;
   protected abstract createUser(context: Context, user: IUserCreateRequest): Promise<T>;
-  protected abstract updateUser(context: Context, user: T, updates: IUserUpdates): Promise<T>;
+  protected abstract updateUser(context: Context, user: T, updates: U): Promise<T>;
 
   async getByEmail(context: Context, email: string) {
     const loginIdentifier = await this.loginIdentifierRepository.get(context, email.toLowerCase());
@@ -30,14 +35,18 @@ export abstract class AbstractUserService<T extends IUser> implements UserServic
     const normalisedEmail = user.email.toLowerCase();
 
     await this.validateEmailAddressAvailable(context, normalisedEmail);
-    const createdUser = await this.createUser(context, {...user, email: normalisedEmail, roles: user.roles || []});
+    const createdUser = await this.createUser(context, {
+      ...user,
+      email: normalisedEmail,
+      roles: user.roles || [],
+    });
     await this.createLoginIdentifier(context, normalisedEmail, createdUser.id);
 
     return createdUser;
   }
 
   @Transactional()
-  async update(context: Context, id: string, updates: IUserUpdates) {
+  async update(context: Context, id: string, updates: U) {
     const user = await this.get(context, id);
     if (!user) {
       throw new Error(`No user exists with id: ${id}`);
@@ -46,11 +55,11 @@ export abstract class AbstractUserService<T extends IUser> implements UserServic
     if (normalisedEmail && normalisedEmail !== user.email) {
       await this.validateEmailAddressAvailable(context, normalisedEmail);
       await Promise.all([
-          this.loginIdentifierRepository.delete(context, user.email),
-          this.createLoginIdentifier(context, normalisedEmail, user.id),
+        this.loginIdentifierRepository.delete(context, user.email),
+        this.createLoginIdentifier(context, normalisedEmail, user.id),
       ]);
     }
-    const userUpdates = (normalisedEmail && {...updates, email: normalisedEmail}) || updates;
+    const userUpdates = (normalisedEmail && ({ ...(updates as object), email: normalisedEmail } as U)) || updates;
     return this.updateUser(context, user, userUpdates);
   }
 
@@ -68,5 +77,4 @@ export abstract class AbstractUserService<T extends IUser> implements UserServic
       throw new Error(`Email address already exists: ${email}`);
     }
   }
-
 }
