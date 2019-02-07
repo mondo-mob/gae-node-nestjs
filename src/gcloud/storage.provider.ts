50bf1108828a3fb83b6314a13a3d628b0bf6a205
Glenn Molnar <glenn@3wks.com.au>
2019-02-07 16:53:05 +1100

    update google libs and typescript to latest

--- src/gcloud/storage.provider.ts
+++ src/gcloud/storage.provider.ts
@@ -1,8 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Configuration } from '../index';
-import * as GoogleCloudStorage from '@google-cloud/storage';
 import * as Logger from 'bunyan';
-import { Bucket, ConfigurationObject, Storage } from '@google-cloud/storage';
+import { Bucket, StorageOptions, Storage } from '@google-cloud/storage';
 import { createLogger } from './logging';
 
 @Injectable()
@@ -13,14 +12,14 @@ export class StorageProvider {
 
   constructor(@Inject('Configuration') private readonly configurationProvider: Configuration) {
     this.logger = createLogger('storage');
-    const config: ConfigurationObject = {};
+    const config: StorageOptions = {};
     if (configurationProvider.isDevelopment()) {
       this.logger.info(
         'Application is running locally, using keyfile.json credentials to connect to Google Cloud Storage',
       );
       config.keyFilename = './keyfile.json';
     }
-    this._storage = GoogleCloudStorage(config);
+    this._storage = new Storage(config);
     this.logger.info(
       `Default Google Cloud Storage bucket: ${configurationProvider.bucket}`,
     );
